c35e1f5db48590f8c77657f7dde28db5dfb66667
emersion <contact@emersion.fr>
2017-03-05 14:10:16 +0100

    Initial release

---
+++ cli.js
@@ -0,0 +1,44 @@
+const http = require('http')
+const fs = require('fs')
+const stream = require('stream')
+
+const serveStatic = require('serve-static')
+const SpliceStream = require('streams2-splice')
+const MultiStream = require('multistream')
+
+const sourcePath = process.argv[2]
+if (!sourcePath) {
+	console.log('Usage: remark-server source.md')
+	process.exit(1)
+}
+
+const templatePath = require.resolve('remark/boilerplate-single.html')
+
+const serve = serveStatic('.')
+
+const server = http.createServer((req, res) => {
+	serve(req, res, () => {
+		const template = fs.createReadStream(templatePath)
+
+		const splice = new SpliceStream('<textarea id="source">', '</textarea>', (s) => {
+			return new stream.Readable().wrap(MultiStream([
+				stringStream('<textarea id="source">'),
+				fs.createReadStream(sourcePath),
+				stringStream('</textarea>')
+			]))
+		})
+
+		template.pipe(splice).pipe(res)
+	})
+})
+
+server.listen(process.env.PORT || 3000, () => {
+	console.log('Server listening on http://localhost:' + server.address().port)
+})
+
+function stringStream(str) {
+	const s = stream.Readable()
+	s.push(str)
+	s.push(null) // Indicates EOF
+	return s
+}
